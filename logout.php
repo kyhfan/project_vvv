cb984dc1959499a767e7ae039cfcdb4902f2cf05
kyhfan <kyhfan@gmail.com>
2017-11-02 17:38:21 +0900

    1122

--- logout.php
+++ logout.php
@@ -1,11 +1,11 @@
 <?
 	include_once "./header.php";
 
-    if ($_SESSION['ss_chon_way'] == "kakao")
+    if ($_SESSION['ss_vvv_way'] == "kakao")
     {
         session_destroy();
         echo "<script>Kakao.Auth.logout();location.href='index.php';</script>";
-    }else if ($_SESSION['ss_chon_way'] == "facebook"){
+    }else if ($_SESSION['ss_vvv_way'] == "facebook"){
         session_destroy();        
         echo "<script>location.href='index.php';</script>";
     }
