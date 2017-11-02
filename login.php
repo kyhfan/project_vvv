cbe35bbe5dd641649b9a2af814a75e009e9c2278
kyhfan <kyhfan@gmail.com>
2017-11-02 16:45:21 +0900

    로그인 작업 중

--- login.php
+++ login.php
@@ -25,15 +25,15 @@
 								<hr class="line_ys">
 								<div class="wrap-buttons">
 									<div class="button">
-										<a href="#">
-											<img src="./images/kakao.png" alt="카카오 로그인" class="mobile-layout">
+										<a href="javascript:loginWithKakao()">
+											<img src="./images/kakao.jpg" alt="카카오 로그인" class="mobile-layout">
 											<img src="./images/kakao_desktop.jpg" alt="카카오 로그인" class="desktop-layout">
 										</a>
 									</div>
 									<div class="button">
 										<a href="#">
-											<img src="./images/naver.png" alt="네이버 로그인" class="mobile-layout">
-											<img src="./images/naver_desktop.jpg" alt="네이버 로그인" class="desktop-layout">
+											<img src="./images/facebook.jpg" alt="페이스북 로그인" class="mobile-layout">
+											<img src="./images/facebook_desktop.jpg" alt="페이스북 로그인" class="desktop-layout">
 										</a>
 									</div>
 								</div>
@@ -52,6 +52,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="./js/TweenMax.js"></script>
+		<script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>		
 		<script type="text/javascript" src="./js/main.js"></script>
 	</body>
 </html>
