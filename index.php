14b1ce6defd4684316b0ee859d5b3a95b0374335
kyhfan <kyhfan@gmail.com>
2017-11-02 16:55:34 +0900

    1111

--- index.php
+++ index.php
@@ -110,7 +110,6 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="./js/TweenMax.js"></script>
-		<script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>		
 		<script type="text/javascript" src="./js/main.js"></script>
 	</body>
 </html>
