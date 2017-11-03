12ffa9c97edddca51271a123892a3633812b5f36
kyhfan <kyhfan@gmail.com>
2017-11-03 20:31:42 +0900

    111

--- logout.php
+++ logout.php
@@ -5,6 +5,7 @@
     {
         session_destroy();
         echo "<script src='//developers.kakao.com/sdk/js/kakao.min.js'></script>";
+        echo "<script>Kakao.init('ff013671b5f7b01d59770657a8787952');</script>";
         echo "<script>Kakao.Auth.logout();
         location.href='./index.php';</script>";
     }else if ($_SESSION['ss_vvv_way'] == "facebook"){
