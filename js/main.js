9b25d52b8f18c760d8fde651c6140da4510b7190
kyhfan <kyhfan@gmail.com>
2017-11-03 15:26:41 +0900

    1111

--- js/main.js
+++ js/main.js
@@ -212,7 +212,7 @@
                     if (response.match("Y") == "Y")
                     {
                         $("#like_img").html("liked");
-                        $("#like_count").html($("#like_count").html() + 1);
+                        $("#like_count").html(Number($("#like_count").html()) + 1);
                     }else{
                         $("#like_img").html("like");
                         $("#like_count").html($("#like_count").html() - 1);
