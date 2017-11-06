66bbb5ff5a9b01e24df8f7086cdb2e5bdf509d50
kyhfan <kyhfan@gmail.com>
2017-11-06 15:47:14 +0900

    임시 약관 파일

---
+++ privacy_agree.php
@@ -0,0 +1,218 @@
+<?
+	include_once "./header.php";
+
+	if ($_SESSION['ss_vvv_email'])
+		echo "<script>location.href='index.php';</script>";
+
+?>
+	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NH7CPGH"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+		<div id="vvv" class="">
+			<div class="bg-layer main">
+				<div class="container">
+					<div class="desktop-layout"></div>
+<?
+	include_once "./head_area.php";
+?>				
+					<div class="content member">
+					개인정보 취급방침
+					
+					
+					'미니버타이징' 은 (이하 '회사'는) 고객님의 개인정보를 중요시하며, "정보통신망 이용촉진 및 정보보호"에 관한 법률을 준수하고 있습니다.
+					회사는 개인정보취급방침을 통하여 고객님께서 제공하시는 개인정보가 어떠한 용도와 방식으로 이용되고 있으며, 개인정보보호를 위해 어떠한 조치가 취해지고 있는지 알려드립니다. 회사는 개인정보취급방침을 개정하는 경우 웹사이트 공지사항(또는 개별공지)을 통하여 공지할 것입니다.
+					ο 본 방침은 : 2017 년 11 월 7 일 부터 시행됩니다.
+					■ 수집하는 개인정보 항목
+					가. 회사는 회원가입, 원활한 고객상담, 각종 서비스의 제공을 위해 최초 회원가입 당시 아래와 같은 개인정보를 수집하고 있습니다. 
+					- 이름, 이메일 
+					또한 서비스 이용과정이나 사업 처리 과정에서 아래와 같은 정보들이 생성되어 수집될 수 있습니다.
+					- 서비스 이용기록, 접속 로그, 쿠키, 접속 IP 정보, 결제기록, 이용정지 기록
+					나. 개인정보 수집방법
+					회사는 다음과 같은 방법으로 개인정보를 수집합니다.
+					- 홈페이지, 서면양식, 팩스, 전화, 상담 게시판, 이메일, 이벤트 응모, 배송요청 
+					- 협력회사로부터의 제공 
+					다. 부가 서비스 및 맞춤식 서비스 이용 과정에서 해당 서비스의 이용자에 한해서만 아래와 같은 정보들이 수집될 수 있습니다. 
+					- 주소, 연락처, 사용 이동통신사, 계좌번호 등
+					■ 개인정보의 수집 및 이용목적
+					가. 서비스 제공에 관한 계약 이행 및 서비스 제공에 따른 요금정산
+					컨텐츠 제공, 특정 맞춤 서비스 제공, 물품배송 또는 청구서 등 발송, 본인인증, 구매 및 요금 결제, 요금추심
+					나. 회원관리
+					회원제 서비스 이용 및 제한적 본인 확인제에 따른 본인확인, 개인식별, 불량회원(다울이용약관 제20조 1항중 제11조 1항 1호~8호 위반사유로 인한 영구이용정지 및 2항에 따라 계약해지된 영구이용정지 회원)의 부정 이용방지와 비인가 사용방지, 가입의사 확인, 가입 및 가입횟수 제한, 만14세 미만 아동 개인정보 수집 시 법정 대리인 동의여부 확인, 추후 법정 대리인 본인확인, 분쟁 조정을 위한 기록보존, 불만처리 등 민원처리, 고지사항 전달
+					다. 신규 서비스 개발 및 마케팅·광고에의 활용
+					신규 서비스 개발 및 맞춤 서비스 제공, 통계학적 특성에 따른 서비스 제공 및 광고 게재, 서비스의 유효성 확인, 이벤트 및 광고성 정보 제공 및 참여기회 제공, 접속빈도 파악, 회원의 서비스이용에 대한 통계
+					■ 개인정보의 보유 및 이용기간
+					이용자의 개인정보는 원칙적으로 개인정보의 수집 및 이용목적이 달성되면 지체 없이 파기합니다. 단, 다음의 정보에 대해서는 아래의 이유로 명시한 기간 동안 보존합니다.
+					
+					가. 회사 내부 방침에 의한 정보보유 사유
+					- 부정이용기록 
+					보존 이유 : 부정 이용 방지 
+					보존 기간 : 1년 
+					나. 관련법령에 의한 정보보유 사유
+					상법, 전자상거래 등에서의 소비자보호에 관한 법률 등 관계법령의 규정에 의하여 보존할 필요가 있는 경우 회사는 관계법령에서 정한 일정한 기간 동안 회원정보를 보관합니다. 이 경우 회사는 보관하는 정보를 그 보관의 목적으로만 이용하며 보존기간은 아래와 같습니다.
+					- 계약 또는 청약철회 등에 관한 기록 
+					보존 이유 : 전자상거래 등에서의 소비자보호에 관한 법률 
+					보존 기간 : 5년 
+					- 대금결제 및 재화 등의 공급에 관한 기록 
+					보존 이유 : 전자상거래 등에서의 소비자보호에 관한 법률 
+					보존 기간 : 5년 
+					- 소비자의 불만 또는 분쟁처리에 관한 기록 
+					보존 이유 : 전자상거래 등에서의 소비자보호에 관한 법률 
+					보존 기간 : 3년 
+					- 본인확인에 관한 기록 
+					보존 이유 : 정보통신망 이용촉진 및 정보보호 등에 관한 법률 
+					보존 기간 : 6개월 
+					- 방문에 관한 기록 
+					보존 이유 : 통신비밀보호법 
+					보존 기간 : 3개월
+					■ 개인정보의 파기절차 및 방법
+					회사는 원칙적으로 개인정보 수집 및 이용목적이 달성된 후에는 해당 정보를 지체없이 파기합니다. 파기절차 및 방법은 다음과 같습니다.
+					①파기절차
+					회원님이 회원가입 등을 위해 입력하신 정보는 목적이 달성된 후 별도의 DB로 옮겨져(종이의 경우 별도의 서류함) 내부 방침 및 기타 관련 법령에 의한 정보보호 사유에 따라(보유 및 이용기간 참조) 일정 기간 저장된 후 파기되어집니다. 별도 DB로 옮겨진 개인정보는 법률에 의한 경우가 아니고서는 보유되어지는 이외의 다른 목적으로 이용되지 않습니다.
+					②파기방법
+					- 전자적 파일형태로 저장된 개인정보는 기록을 재생할 수 없는 기술적 방법을 사용하여 삭제합니다.
+					■ 개인정보 제공
+					회사는 이용자의 개인정보를 원칙적으로 외부에 제공하지 않습니다. 다만, 아래의 경우에는 예외로 합니다.
+					①이용자들이 사전에 동의한 경우
+					②법령의 규정에 의거하거나, 수사 목적으로 법령에 정해진 절차와 방법에 따라 수사기관의 요구가 있는 경우
+					■ 수집한 개인정보의 위탁
+					①회사는 고객님의 동의없이 고객님의 정보를 외부 업체에 위탁하지 않습니다. 향후 그러한 필요가 생길 경우, 위탁 대상자와 위탁 업무 내용에 대해 고객님에게 통지하고 필요한 경우 사전 동의를 받도록 하겠습니다.
+					■ 이용자 및 법정대리인의 권리와 그 행사방법
+					이용자 및 법정 대리인은 언제든지 등록되어 있는 자신 혹은 당해 만 14세 미만 아동의 개인정보를 조회하거나 수정할 수 있으며 가입해지를 요청할 수도 있습니 다. 이용자 혹은 만 14세 미만 아동의 개인정보 조회?수정을 위해서는 ‘개인정보변 경’(또는 ‘회원정보수정’ 등)을 가입해지(동의철회)를 위해서는 “회원탈퇴”를 클릭 하여 본인 확인 절차를 거치신 후 직접 열람, 정정 또는 탈퇴가 가능합니다. 혹은 개인정보관리책임자에게 서면, 전화 또는 이메일로 연락하시면 지체없이 조 치하겠습니다. 귀하가 개인정보의 오류에 대한 정정을 요청하신 경우에는 정정을 완료하기 전까 지 당해 개인정보를 이용 또는 제공하지 않습니다. 또한 잘못된 개인정보를 제3자 에게 이미 제공한 경우에는 정정 처리결과를 제3자에게 지체없이 통지하여 정정이 이루어지도록 하겠습니다. oo는 이용자 혹은 법정 대리인의 요청에 의해 해지 또는 삭제된 개인정보는 “oo 가 수집하는 개인정보의 보유 및 이용기간”에 명시된 바에 따라 처리하고 그 외의 용도로 열람 또는 이용할 수 없도록 처리하고 있습니다.
+					■ 개인정보 자동수집 장치의 설치, 운영 및 그 거부에 관한 사항
+					쿠키 등 인터넷 서비스 이용 시 자동 생성되는 개인정보를 수집하는 장치를 운영하지 않습니다.
+					■ 개인정보에 관한 민원서비스
+					회사는 고객의 개인정보를 보호하고 개인정보와 관련한 불만을 처리하기 위하여 아래와 같이 관련 부서 및 개인정보관리책임자를 지정하고 있습니다.
+					개인정보관리책임자 성명 : 김지훈
+					전화번호 : 02-2068-2903
+					이메일 : bopuragi@dawul.co.kr
+					귀하께서는 회사의 서비스를 이용하시며 발생하는 모든 개인정보보호 관련 민원을 개인정보관리책임자 혹은 담당부서로 신고하실 수 있습니다. 회사는 이용자들의 신고사항에 대해 신속하게 충분한 답변을 드릴 것입니다.
+					기타 개인정보침해에 대한 신고나 상담이 필요하신 경우에는 아래 기관에 문의하시기 바랍니다.
+					1.개인정보침해신고센터 (www.1336.or.kr/국번없이 118)
+					2.정보보호마크인증위원회 (www.eprivacy.or.kr/02-580-0533~4)
+					3.대검찰청 인터넷범죄수사센터 (http://icic.sppo.go.kr/02-3480-3600)
+					4.경찰청 사이버테러대응센터 (www.ctrc.go.kr/02-392-0330)
+					</div>
+<?
+	include_once "./search_area.php";
+	include_once "./footer.php";
+?>
+				</div>
+			</div>
+		</div>
+		<!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./js/TweenMax.js"></script>
+		<script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>		
+<?
+	if ($gubun == "MOBILE")
+	{
+?>		
+		<script type="text/javascript" src="./js/m_main.js"></script>
+<?
+	}else{
+?>		
+		<script type="text/javascript" src="./js/main.js"></script>
+<?
+	}
+?>
+		<script type="text/javascript">
+		Kakao.init('ff013671b5f7b01d59770657a8787952');
+		
+		function getUserData() {
+			/* FB.api('/me', function(response) {
+				document.getElementById('response').innerHTML = 'Hello ' + response.name;
+				console.log(response);
+			}); */
+			FB.api('/me', {fields: 'name,email,gender,birthday'}, function(response) {
+				console.log(JSON.stringify(response));
+				// $("#name").text("이름 : "+response.name);
+				// $("#email").text("이메일 : "+response.email);
+				// $("#gender").text("성별 : "+response.gender);
+				// $("#birthday").text("생년월일 : "+response.birthday);
+				// $("#id").text("아이디 : "+response.id);
+
+				$.ajax({
+					type   : "POST",
+					async  : false,
+					url    : "./main_exec.php",
+					data:{
+						"exec"				: "member_facebook_login",
+						"login_way"			: "facebook",
+						"mb_name"			: response.name,
+						"mb_email"			: response.email,
+						"gender"			: response.gender,
+						"birthday"			: response.birthday,
+						"id"				: response.id
+					},
+					success: function(response){
+						if (response.match("Y") == "Y")
+						{
+							location.href	= "<?=$ref_url?>";
+						}else{
+							alert("다시 시도해 주세요!");
+							location.reload();
+						}
+
+					}
+				});
+
+			});
+		}
+
+		window.fbAsyncInit = function() {
+			//SDK loaded, initialize it
+			FB.init({
+				appId      : '1893328200738001',
+				cookie     : true,  // enable cookies to allow the server to access
+						// the session
+				xfbml      : true,  // parse social plugins on this page
+				version    : 'v2.8' // use graph api version 2.8
+			});
+
+			//check user session and refresh it
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					//user is authorized
+					//document.getElementById('loginBtn').style.display = 'none';
+					//getUserData();
+					FB.logout();
+				} else {
+					//user is not authorized
+				}
+			});
+		};
+
+		//load the JavaScript SDK
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.com/ko_KR/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+
+		//add event listener to login button
+		document.getElementById('fblogin').addEventListener('click', function() {
+			//do the login
+			FB.login(function(response) {
+				if (response.authResponse) {
+					access_token = response.authResponse.accessToken; //get access token
+					user_id = response.authResponse.userID; //get FB UID
+					console.log('access_token = '+access_token);
+					console.log('user_id = '+user_id);
+					$("#access_token").text("접근 토큰 : "+access_token);
+					$("#user_id").text("FB UID : "+user_id);
+					//user just authorized your app
+					//document.getElementById('loginBtn').style.display = 'none';
+					getUserData();
+				}
+			}, {scope: 'email,public_profile,user_birthday',
+				return_scopes: true});
+		}, false);
+		
+		</script>
+	</body>
+</html>
