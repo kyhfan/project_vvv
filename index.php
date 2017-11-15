0b4a3fc5a87ac9381141130b18f21a26dd71b2a0
kyhfan <kyhfan@gmail.com>
2017-11-15 15:25:50 +0900

    1122

--- index.php
+++ index.php
@@ -125,6 +125,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="./js/TweenMax.js"></script>
+		<script type="text/javascript" src="./js/lazyload.min.js"></script>
 <?
 	if ($gubun == "MOBILE")
 	{
