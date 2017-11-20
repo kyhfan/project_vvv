f3e6d7cdc3eb5446b8e6376de899557a07d212f2
kyhfan <kyhfan@gmail.com>
2017-11-20 10:15:37 +0900

    애드센스 코드 삽입

--- header.php
+++ header.php
@@ -40,4 +40,13 @@
 		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 		})(window,document,'script','dataLayer','GTM-NH7CPGH');</script>
 		<!-- End Google Tag Manager -->
+
+		<!-- 애드센스 코드 -->
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({
+			google_ad_client: "ca-pub-4836382194799502",
+			enable_page_level_ads: true
+		});
+		</script>		
 	</head>
--- header_video.php
+++ header_video.php
@@ -55,4 +55,12 @@
 		<script>
 			Kakao.init('ff013671b5f7b01d59770657a8787952');
 		</script>
+		<!-- 애드센스 코드 -->
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({
+			google_ad_client: "ca-pub-4836382194799502",
+			enable_page_level_ads: true
+		});
+		</script>		
 	</head>
